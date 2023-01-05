7b7336c2b63f1e6d9b196dc2a4921160b3a9c8c6
Chameera Mayura <Mayura.chameera@gmail.com>
2023-01-05 11:27:43 +0530

    app styles

--- resources/views/Layout/appStyles.blade.php
+++ resources/views/Layout/appStyles.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8" />
     <title>Awakaza </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Awakaza PMS" name="description" />
+    <meta content="Circlebook" name="author" />
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{URL::asset('assets/images/az.png')}}">
 
--- resources/views/home/loginLayout.blade.php
+++ resources/views/home/loginLayout.blade.php
@@ -1,12 +1,12 @@
 <head>
 
     <meta charset="utf-8" />
-    <title>Awakaza</title>
+    <title>Login | Awakaza-PMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesbrand" name="author" />
+    <meta content="Awakaza PMS" name="description" />
+    <meta content="Circlebook" name="author" />
     <!-- App favicon -->
-    <link rel="shortcut icon" href="{{URL::asset('assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" href="{{URL::asset('assets/images/az.png')}}">
 
     <!-- Bootstrap Css -->
     <link href="{{URL::asset('/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />

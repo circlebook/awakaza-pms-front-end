d6a335d98139b3d5dfe9856df15e14c067ae89e6
Chameera Mayura <Mayura.chameera@gmail.com>
2023-01-05 11:17:56 +0530

    user Settings and login

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,19 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('home.login');
 });
+
+//User
+
+Route::get('/dashboard',[UserController::class,'Dashboard']);
+Route::get('/userManagement',[UserController::class,'UserManagement']);
+Route::post('/addUser',[UserController::class,'AddUser']);
+Route::post('/login',[UserController::class,'Login']);
+Route::get('/logout',[UserController::class,'Logout']);
+Route::get('/changePasswordView{id}',[UserController::class,'ChangePasswordView']);
+Route::post('/changePassword',[UserController::class,'ChangePassword']);
+Route::get('/editProfileView{id}',[UserController::class,'EditProfileView']);
+Route::post('/editProfile',[UserController::class,'EditProfile']);
+Route::post('/resetPassword',[UserController::class,'ResetPassword']);
+Route::get('/disableUser{userId}',[UserController::class,'DisableUser']);
+
+
